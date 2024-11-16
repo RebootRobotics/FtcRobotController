3df825c41a4d4fe6892fb0192d076014060205c8
ScodingV <srikarvem@gmail.com>
2024-11-16 08:14:43 -0800

    updte teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop20241.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop20241.java
@@ -14,6 +14,13 @@ import com.qualcomm.robotcore.hardware.CRServo;
 @TeleOp(name="Teleop20241")
 public class Teleop20241 extends LinearOpMode {
 //    @Override
+
+
+    // Reverse the right side motors. This may be wrong for your setup.
+    // If your robot moves backwards when commanded to go forwards,
+    // reverse the left side instead.
+    // See the note about this earlier on this page.
+
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
@@ -35,16 +42,17 @@ public class Teleop20241 extends LinearOpMode {
         Servo SlidePivot1 = hardwareMap.servo.get("SlidePivot1");
         Servo LiftWrist = hardwareMap.servo.get("LiftWrist");
         Servo VClaw = hardwareMap.servo.get("VClaw");
-
-
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
+//
+//
+//        // Reverse the right side motors. This may be wrong for your setup.
+//        // If your robot moves backwards when commanded to go forwards,
+//        // reverse the left side instead.
+//        // See the note about this earlier on this page.
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         VSlide2.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        intakeClaw.setPosition(0.8);
+//        clawWrist.setPosition(0.0);
 
         waitForStart();
 
@@ -71,55 +79,82 @@ public class Teleop20241 extends LinearOpMode {
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+
+//            clawWrist.setPosition(0.5);
             //VSlide1.setPower(1);
             //VSlide2.setPower(-1);
             //Make So that there are preset heights
 
 
+//            if (gamepad1.a) {
+//                lift1.setPosition(1.0);
+//            } else if (gamepad1.b) {
+//                lift1.setPosition(0.025);
+//            }
+//            //init vals: 0.1, 0.6
+//            if (gamepad1.a) {
+//                lift2.setPosition(0.1);
+//            } else if (gamepad1.b) {
+//                lift2.setPosition(1.9);
+//            }
             if (gamepad1.a) {
-                lift1.setPosition(0.6);
+                lift1.setPosition(1.5);
             } else if (gamepad1.b) {
-                lift1.setPosition(0.1);
+                lift1.setPosition(0.0);
             }
+            //init vals: 0.1, 0.6
             if (gamepad1.a) {
-                lift2.setPosition(0.1);
+                lift2.setPosition(0.05);
             } else if (gamepad1.b) {
-                lift2.setPosition(0.6);
+                lift2.setPosition(1.0);
             }
 
+            if (gamepad1.x) {
+                lift1.setPosition(0.6);
+                lift2.setPosition(0.1);
+                sleep(2000);
+                VClaw.setPosition(0.1);
+                sleep(2000);
+                intakeClaw.setPosition(-0.5);
+//                transfer();
+            }
+//liftwrist and vclaw switch
             if (gamepad2.dpad_left) {
-                SlidePivot2.setPosition(0.1);
+                SlidePivot2.setPosition(0.9);
             } else if (gamepad2.dpad_right) {
-                SlidePivot2.setPosition(0.45);
+                SlidePivot2.setPosition(0.1);
             }
             if (gamepad2.dpad_right) {
-                SlidePivot1.setPosition(0.45);
+                SlidePivot1.setPosition(0.9);
             } else if (gamepad2.dpad_left) {
                 SlidePivot1.setPosition(0.1);
             }
             if (gamepad2.dpad_up) {
-                LiftWrist.setPosition(0.1);
-            } else if (gamepad2.dpad_down) {
-                LiftWrist.setPosition(0.3);
-            }
-            if (gamepad2.right_bumper) {
-                VClaw.setPosition(0.1);
-            } else if (gamepad2.left_bumper) {
                 VClaw.setPosition(0.5);
+            } else if (gamepad2.dpad_down) {
+                VClaw.setPosition(0.1);
             }
+            //LIFTWRIST AND VCLAW ARE SWITCH
+            if (gamepad2.right_bumper) {
+                LiftWrist.setPosition(0.1);
+            } else if (gamepad2.left_bumper) {
+                LiftWrist.setPosition(0.5);
+            }
+            //vclaw == liftwrist here bc of server confusion for some reason (keep for now)
+            //suposed to be claw in black (outtake claw)
 
 
             if (gamepad1.left_bumper) {
-                intakeClaw.setPosition(0.5);
+                intakeClaw.setPosition(0.9);
             } else if (gamepad1.right_bumper) {
                 intakeClaw.setPosition(0.1);
             }
 
-            float leftTriggerValue = gamepad1.left_trigger;
-            float rightTriggerValue = gamepad1.right_trigger;
-
-            clawWrist.setPosition(leftTriggerValue);
-            clawWrist.setPosition(rightTriggerValue);
+//            float leftTriggerValue = gamepad1.left_trigger;
+//            float rightTriggerValue = gamepad1.right_trigger;
+//
+//            clawWrist.setPosition(leftTriggerValue);
+//            clawWrist.setPosition(rightTriggerValue);
             //if (gamepad1.dpad_up) {
             //clawWrist.getPosition();
             //clawWrist.setPosition(clawWrist.getPosition()+0.1);
@@ -141,19 +176,40 @@ public class Teleop20241 extends LinearOpMode {
                 extension2.setPosition(0.7);
             }
 
+//            if (gamepad2.y){
+//                VSlide1.setPower(0.4);
+//                VSlide2.setPower(-0.4);}
+//              else if (!gamepad2.y) {
+//                VSlide1.setPower(0);
+//                VSlide2.setPower(0);
+//            }
+//
+//            if(gamepad2.x){
+//                VSlide1.setPower(-0.4);
+//                VSlide2.setPower(0.4);
+//            }
             if (gamepad2.y){
-                VSlide1.setPower(0.4);
-                VSlide2.setPower(-0.4);}
-              else if (!gamepad2.y) {
-                VSlide1.setPower(0);
-                VSlide2.setPower(0);
+                VSlide1.setPower(-0.6);
+                VSlide2.setPower(0.6);}
+//reverse
+            else if(gamepad2.x){
+                VSlide1.setPower(0.9);
+                VSlide2.setPower(-0.9);
             }
-
-            if(gamepad2.x){
-                VSlide1.setPower(-0.4);
-                VSlide2.setPower(0.4);
-            }
+            //positive
 
 
         }
-    }}
+
+        }
+//    public void transfer(){
+//
+//        lift1.setPosition(0.6);
+//        lift2.setPosition(0.1);
+//        sleep(2000);
+//        VClaw.setPosition(0.1);
+//        sleep(2000);
+//        intakeClaw.setPosition(-0.5);
+//        }
+
+    }
